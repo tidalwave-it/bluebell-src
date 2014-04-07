ce556f66a632620b34c9e3d79449839f2aded6d0
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-04-07 23:09:55 +0200

    Fixes in dependencies.

--- modules/blueBell-it/src/main/java/it/tidalwave/bluebell/bluebell/test/HelloAndroidActivityTest.java
+++ modules/blueBell-it/src/main/java/it/tidalwave/bluebell/bluebell/test/HelloAndroidActivityTest.java
@@ -1,16 +1,17 @@
 package it.tidalwave.bluebell.bluebell.test;
 
 import android.test.ActivityInstrumentationTestCase2;
-import it.tidalwave.bluebell.mobile.SampleCameraActivity;
+import it.tidalwave.bluebell.cameradiscovery.impl.android.CameraDiscoveryPresentationActivity;
 
-public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<SampleCameraActivity> {
+public class HelloAndroidActivityTest extends ActivityInstrumentationTestCase2<CameraDiscoveryPresentationActivity> {
 
     public HelloAndroidActivityTest() {
-        super("it.tidalwave.bluebell.bluebell", SampleCameraActivity.class);
+//        super("it.tidalwave.bluebell.bluebell", CameraDiscoveryPresentationActivity.class);
+        super(CameraDiscoveryPresentationActivity.class);
     }
 
     public void testActivity() {
-        SampleCameraActivity activity = getActivity();
+        CameraDiscoveryPresentationActivity activity = getActivity();
         assertNotNull(activity);
     }
 }
