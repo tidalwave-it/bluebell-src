0cdfc5b363dc9a4ef1713b70cf6ba13764e900b7
Fabrizio Giudici <Fabrizio.Giudici@tidalwave.it>
2014-11-16 17:16:15 +0100

    Clean up.

--- modules/blueBell/src/main/java/it/tidalwave/bluebell/cameraview/impl/android/CameraPresentationActivity.java
+++ modules/blueBell/src/main/java/it/tidalwave/bluebell/cameraview/impl/android/CameraPresentationActivity.java
@@ -505,12 +505,6 @@ public class CameraPresentationActivity extends Activity implements CameraPresen
     public void onFNumberClicked (final @Nonnull View view)
       {
         control.changeProperty(CameraObserver.Property.F_NUMBER);
-        
-        
-//        final String value = tvFNumber.getText().toString().replaceAll("F", "");
-//        // FIXME: retrieve valid values from the camera
-//        final List<String> values = Arrays.asList("4.0","4.5","5.0","5.6","6.3","7.1","8.0","9.0","10","11","13","14","16","18","20","22");
-//        setProperty(CameraApi.Property.F_NUMBER, value, values);
       }
     
     /*******************************************************************************************************************
@@ -521,12 +515,6 @@ public class CameraPresentationActivity extends Activity implements CameraPresen
     public void onShutterSpeedClicked (final @Nonnull View view)
       {
         control.changeProperty(CameraObserver.Property.SHUTTER_SPEED);
-        
-        
-//        final String value = tvShutterSpeed.getText().toString();
-//        // FIXME: retrieve valid values from the camera
-//        final List<String> values = Arrays.asList("30\"","25\"","20\"","15\"","13\"","10\"","8\"","6\"","5\"","4\"","3.2\"","2.5\"","2\"","1.6\"","1.3\"","1\"","0.8\"","0.6\"","0.5\"","0.4\"","1/3","1/4","1/5","1/6","1/8","1/10","1/13","1/15","1/20","1/25","1/30","1/40","1/50","1/60","1/80","1/100","1/125","1/160","1/200","1/250","1/320","1/400","1/500","1/640","1/800","1/1000","1/1250","1/1600","1/2000","1/2500","1/3200","1/4000");
-//        setProperty(CameraApi.Property.SHUTTER_SPEED, value, values);
       }
     
     /*******************************************************************************************************************
@@ -537,12 +525,6 @@ public class CameraPresentationActivity extends Activity implements CameraPresen
     public void onIsoSpeedRateClicked (final @Nonnull View view)
       {
         control.changeProperty(CameraObserver.Property.ISO_SPEED_RATE);
-        
-        
-//        final String value = tvIsoSpeedRate.getText().toString().replaceAll("ISO ", "");
-//        // FIXME: retrieve valid values from the camera
-//        final List<String> values = Arrays.asList("100","200","400","800","1600","3200","6400","12800","25600");
-//        setProperty(CameraApi.Property.ISO_SPEED_RATE, value, values);
       }
     
     /*******************************************************************************************************************
